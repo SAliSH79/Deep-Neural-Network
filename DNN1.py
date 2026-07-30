1dc682cd8cb95b1a4edf8645b02fc2a48eae8419
SeyedAli SeyedHosseini <alihosseini797979@gmail.com>
2026-07-30 22:32:20 +0200

    DNN 1st 3 layers Keras

---
+++ DNN1.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+# Input features
+input_features = 20
+
+# Build a 3 layers Deep Neural Network model
+
