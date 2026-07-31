801cac8c5f99e29914c7ae112d9fcf8aaed2a3e9
SeyedAli SeyedHosseini <alihosseini797979@gmail.com>
2026-07-31 03:02:57 +0200

    Sequential while simple stack of layers

--- DNN1.py
+++ DNN1.py
@@ -6,4 +6,9 @@ from tensorflow.keras.layers import Dense
 input_features = 20
 
 # Build a 3 layers Deep Neural Network model
+model = Sequential(
+    Dense(64, activation="relu", input_shape=(input_features,)) #Hidden Layer 1
+    Dense(32, activation="relu"), #Hidden Layer 2
+    Dense(1, activation="sigmoid") #Output layer
 
+)
